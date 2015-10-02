49be60f1705169a8bb5490949df78367126e5c69
DagInge <dihelgoy@gmail.com>
2015-10-02 12:04:58 +0200

    Restructured views and controllers

--- app/app.js
+++ app/app.js
@@ -3,10 +3,15 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+      .when('/view1', {
+        templateUrl: 'views/view1.html',
+        controller: 'controllers/view1'
+      })
+      .when('/view2', {
+        templateUrl: 'views/view2.html',
+        controller: 'controllers/view2'
+      });
 }]);
---
+++ app/controllers/view1.js
@@ -0,0 +1,3 @@
+app.controller('view1', function($scope) {
+
+});
