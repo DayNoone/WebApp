4f596f122b125eb344e362a2a6c2b822a2ffd5a8
Kristian <huskrist@hotmail.com>
2015-10-08 12:21:44 +0200

    Endret litt på utforming i testfasen, bare for å få litt større bilde. La til kategori og filter på hver side av kartet. Endret også mapsAPIet, slik at vi kan gjøre stedssøk.

--- app/controllers/view2.js
+++ app/controllers/view2.js
@@ -1,9 +1,71 @@
 app.controller('view2', function($scope) {
-    var mapOptions = {
+    var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
         center: new google.maps.LatLng(63.4174652, 10.4043239),
         mapTypeId: google.maps.MapTypeId.TERRAIN
+    });
+
+    $scope.currentlySelected = "Ingenting";
+
+    $scope.selected = function(itemId) { // Tenke å kanskje vise ikoner på det man har valgt
+        $scope.currentlySelected = itemId;
+        console.log(itemid);
     }
 
-    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    /* Google Maps Search Code, Gotten from Google Developers */
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('searchField');
+    var searchBox = new google.maps.places.SearchBox(input);
+   // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+    // END OF GOOGLE DEVELOPER CODE
 });
