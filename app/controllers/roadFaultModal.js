3ebe36d236d4e01c1c37baf3f4857c8587a5f69c
Kristian <huskrist@hotmail.com>
2015-10-21 16:04:45 +0200

    Added view for error description

--- app/controllers/roadFaultModal.js
+++ app/controllers/roadFaultModal.js
@@ -1,4 +1,9 @@
 app.controller("roadFaults", function($scope, obj) {
     $scope.selectedCluster= obj;
     console.log(obj);
+
+
+
+    $scope.dataClicked = {"userId":39,"lat":63.42945947,"lon":10.39348956,"timestamp":"09/10/2015/00:04:34", "image": 'resources/voldemort.png', "description":"Problem description: He who's face must have an error of 404: nose not found."};
+
 });
